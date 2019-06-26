4716c979fd095ff199c84f4ca7966e632851fd3d
Potzerus <Potzerus@gmail.com>
2019-06-26 21:30:32 +0200

    Added Descriptions to Existing commands

--- src/main/java/DroneModule.java
+++ src/main/java/DroneModule.java
@@ -10,14 +10,20 @@ public class DroneModule extends Module {
     public DroneModule(String prefix, DiscordApi api, Server server, State state) {
         super(prefix, api, server, state);
         commandMap.registerAll(
-                new Test("test"),
-                new Check("check"),
-                new Generate("generate"),
-                new Rename("rename"),
-                new Select("select"),
-                new Browse("browse"),
-                new ToggleId("toggleid"),
-                new Scream("scream")
+                new Check("check",
+                        "Used by itself to see your drones, and with ``drone`` to see details about your selected Drone"),
+                new Generate("generate",
+                        "Generates a starting drone, if you already have one you need to add ``reset`` to reset your current drones in exchange for a default one"),
+                new Rename("rename",
+                        "Renames your currently Selected Drone"),
+                new Select("select",
+                        "Lets you select a drone from your owned ones, enter either its ID or its name. ``"+prefix+" check`` to see your current Drones"),
+                new Browse("browse",
+                        "Used to take a more thorough look into the makeup of your Drone"),
+                new ToggleId("toggleid",
+                        "Toggles wether you will see your Drone's Id's or not"),
+                new Scream("scream",
+                        "Testing Method please ignore")
         );
     }
 
