67fb36352db6f9a5a6c6b090c9d1198848db1984
11812327 <e11812327@student.tuwien.ac.at>
2019-06-27 18:15:12 +0200

    New Class Blueprint and Trait. WiP

---
+++ src/main/java/drone/blueprints/Blueprint.java
@@ -0,0 +1,16 @@
+package drone.blueprints;
+
+import drone.component.ComponentType;
+
+public class Blueprint {
+
+    private int[] cost;
+    private ComponentType type;
+    private Trait trait;
+
+
+    public Blueprint() {
+
+    }
+
+}
---
+++ src/main/java/drone/blueprints/Trait.java
@@ -0,0 +1,15 @@
+package drone.blueprints;
+
+import drone.component.ComponentType;
+
+import java.util.HashSet;
+
+public class Trait {
+
+    private HashSet<ComponentType> compatible;
+
+    public Trait() {
+
+    }
+
+}
