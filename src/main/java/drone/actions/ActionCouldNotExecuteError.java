424af82a0a5014604968349d0804c073c1104c14
Potzerus <Potzerus@gmail.com>
2019-07-04 23:26:46 +0200

    Spacefix

--- src/main/java/drone/actions/ActionCouldNotExecuteError.java
+++ src/main/java/drone/actions/ActionCouldNotExecuteError.java
@@ -3,7 +3,7 @@ package drone.actions;
 import launcher.Center;
 import potz.utils.database.Char;
 
-public class ActionCouldNotExecuteError extends Error {
+public class    ActionCouldNotExecuteError extends Error {
     public ActionCouldNotExecuteError(String message, Char player){
         super(message);
         Center.getApi().getUserById(player.getId()).join().openPrivateChannel().join().sendMessage(
